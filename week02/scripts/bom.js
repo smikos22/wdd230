00e62604c3e772c0a65d03b6136713b18343c081
smikos22 <mik22003@byui.edu>
2023-01-08 18:14:49 -0700

    bom js

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,4 +1,30 @@
-const input = ""
-const button = document.button
-button.addEventListener('click', function() { ... })
-const list = ""
+const list = document.querySelector('ul')
+const input = document.querySelector('input')
+
+const button = document.querySelector('button')
+button.addEventListener('click', () => { 
+    
+    const myItem = input.value;
+
+    if (myItem == '') {
+        input.focus();
+    }
+    else {
+        input.value = '';
+        const listItem = document.createElement('li');
+        const listText = document.createElement('span');
+        const listBtn = document.createElement('button');
+
+        listItem.appendChild(listText);
+        listText.textContent = myItem;
+        listItem.appendChild(listBtn);
+        listBtn.textContent = 'Delete' ;
+        list.appendChild(listItem);
+
+        listBtn.addEventListener('click', () => {
+        list.removeChild(listItem);
+        });
+
+        input.focus();
+    }
+})
