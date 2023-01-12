11ff56d135f03bdaefc3f1e8f061475344fce378
smikos22 <mik22003@byui.edu>
2023-01-11 20:08:18 -0700

    dark toggle

---
+++ chamber/scripts/getDates1.js
@@ -0,0 +1,31 @@
+console.log("It is now "+currentDateAndTime)
+
+
+let theTotal = total(1,2,3,4,5,6,7,8,9,10)
+
+console.log("The total is "+theTotal)
+
+
+function total(...theNumbers){
+	let sum = 0
+	for(let aNumber in theNumbers){
+		sum += aNumber*1//Want to know why aNumber is multiplied by 1? Remove it and find out. :)
+	}
+	return sum
+}
+
+
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("☑️")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "❎";
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.textContent = "☑️";
+	}
+});
