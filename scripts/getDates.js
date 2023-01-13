e229c3ef63bae15fa962561e77a2f145baeb84f9
smikos22 <mik22003@byui.edu>
2023-01-12 18:26:54 -0700

    dark toggle to main page

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -16,6 +16,20 @@ function total(...theNumbers){
 	return sum
 }
 
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("☑️")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "❎";
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.textContent = "☑️";
+	}
+});
 
 
 
