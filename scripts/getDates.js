6dbccadcc2de1585c1e3647342b6137176070d80
smikos22 <mik22003@byui.edu>
2023-01-12 19:16:16 -0700

    responsive menu; don't like nav section

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -31,8 +31,15 @@ modeButton.addEventListener("click", () => {
 	}
 });
 
+// Store the selected elements that we are going to use. 
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
 
+// Add a simple arrow function that listens for the <li> hamburger button click event.
+// When clicked, the <ul class="navigation">'s class list toggle'
+hambutton.addEventListener('click', () => {mainnav.classList.toggle('responsive')}, false);
 
+// What does toggle mean? Instead of add and remove, toggle means add the class name (the parameter, which in this case is named 'responsive') if it does not currently exist, and remove the 
 
 /*const options = {weeday: 'long', day: 'numberic', month: 'long', year:'numeric'};
 document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-Us', options);*/
