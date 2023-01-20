6200dc8261c51a5e6f845bfa2cd8be976d0693d8
smikos22 <mik22003@byui.edu>
2023-01-19 19:51:57 -0700

    discover css

--- chamber/scripts/getDates1.js
+++ chamber/scripts/getDates1.js
@@ -1,3 +1,4 @@
+let currentDateAndTime = Date()
 console.log("It is now "+currentDateAndTime)
 
 
@@ -29,3 +30,23 @@ modeButton.addEventListener("click", () => {
 		modeButton.textContent = "☑️";
 	}
 });
+
+const todayDisplay = document.querySelector(".today");
+const visitsDisplay = document.querySelector(".visits");
+
+// get the stored value in localStorage
+let numVisits = Number(window.localStorage.getItem("visits-ls"));
+
+// determine if this is the first visit or display the number of visits.
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit!`;
+}
+
+// increment the number of visits.
+numVisits++;
+// store the new number of visits value
+localStorage.setItem("visits-ls", numVisits);
+// show todays date.
+todayDisplay.textContent = Date.now();
