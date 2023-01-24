7e36225fc7368e504e27666772ce05c1ffed8ff0
smikos22 <mik22003@byui.edu>
2023-01-23 20:36:40 -0700

    contact form

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -65,3 +65,35 @@ localStorage.setItem("visits-ls", numVisits);
 todayDisplay.textContent = Date.now();
 
 // You can view the localStorage data using the Applications panel in the browsers's DevTools.
+//form//
+const rating = document.getElementById("rating");
+const rangevalue = document.getElementById("r");
+
+function displayRatingValue() {
+    rating.innerHTML = rangevalue.value;
+}
+
+rangevalue.addEventListener('change', displayRatingValue);
+rangevalue.addEventListener('input', displayRatingValue);
+//key phrase
+const kp1 = document.querySelector("#keyphrase");
+const kp2 = document.querySelector("#keyphrase2");
+const message = document.querySelector("#formmessage");
+
+kp2.addEventListener("focusout", checkSame);
+
+// This should be refactored.
+function checkSame() {
+	if (kp1.value !== kp2.value) {
+		message.textContent = "Key Phrases DO NOT MATCH!";
+		message.style.display = "block";
+		kp2.style.backgroundColor = "#aa3333";
+		kp2.style.color = "#fff";
+		kp2.focus();
+		kp2.value = "";
+	} else {
+		message.style.display = "none";
+		kp2.style.backgroundColor = "#fff";
+		kp2.style.color = "#000";
+	}
+}
