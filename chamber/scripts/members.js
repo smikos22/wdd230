bf01eb36443c2d2fed9443a43797666affcbd8e7
smikos22 <mik22003@byui.edu>
2023-02-10 12:40:26 -0700

    Gage wants to look at this case.

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -4,7 +4,14 @@ async function loadData() {
 	
 	const jsonObj = JSON.parse(jsonString);
 	const section = document.querySelector("#business");
-	const aTags = section.querySelectorAll("a");
+    const randomBusinessName = document.querySelector("#name");
+    const randomBusinessImage = document.querySelector("#image");
+    const randomBusinessMembershipLevel = document.querySelector("#membershipLevel");
+    const randomBusinessPhoneNumber = document.querySelector("#phoneNumber");
+    const randomBusinessUrl = document.querySelector("#url");
+//    const randomBusinessAddress = document.querySelector("#address");
+
+//	const aTags = section.querySelectorAll("a");
 /*	
 	//loop through <a> tags
 	let count = 0
@@ -15,16 +22,30 @@ async function loadData() {
 		count += 1
 
 	*/	
-	count = 0
+	//count = 0
+    let randomNumber = 0
 	for (key in jsonObj){
-		for (value in jsonObj[key])
-		{
-			console.log(count)
-			console.log(aTags[count])
-			console.log(jsonObj[key][value])
-			aTags[count].href= jsonObj[key][value]
-			count += 1;
-		}
+        console.log(jsonObj[key][randomNumber])
+        let randomBusiness = jsonObj[key][randomNumber]
+        randomBusinessName.innerHTML = randomBusiness["name"]
+        randomBusinessAddress.innerHTML = randomBusiness["address"]
+        console.log(randomBusinessAddress)
+        randomBusinessPhoneNumber.innerHTML = randomBusiness["phoneNumber"]
+        randomBusinessMembershipLevel.innerHTML = randomBusiness["membershipLevel"]
+        randomBusinessImage.setAttribute("src", randomBusiness["image"])
+        randomBusinessUrl.innerHTML = randomBusiness["url"]
+        randomBusinessUrl.setAttribute("href", randomBusiness["url"])
+        //console.log(randomBusinessUrl)
+        //randomBusinessUrl.innerHTML = randomBusiness["url"]
+
+		//for (value in jsonObj[key])
+		//{
+			//console.log(count)
+//			console.log(aTags[count])
+			//console.log(jsonObj[key][value])
+//			//aTags[count].href= jsonObj[key][value]
+			//count += 1;
+		//}
 	}
 	return;
 }
